6b9df7f506ff591043e29d7470fdbdd284be9de0
Takeru9016 <youthfox046@gmail.com>
2023-05-28 21:06:34 +0530

    feat:Finished with fetching listings from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-export default function Home() {
+import { getCurrentUser, getListings } from "@/actions";
+import { ClientOnly, Container, EmptyState, ListingCard } from "@/components";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div>Hello Airbnb!</div>
-  )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
---
+++ src/components/HeartButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { SafeUser } from "@/types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface HeartButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+export default function HeartButton({
+  listingId,
+  currentUser,
+}: HeartButtonProps) {
+  const hasFavorite = false;
+  const toggleFavorite = () => {};
+
+  return (
+    <div
+      className="relative hover:opacity-80 transition cursor-pointer"
+      onClick={toggleFavorite}
+    >
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorite ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
+}
