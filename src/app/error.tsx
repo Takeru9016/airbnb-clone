65b7c2279f3603b74957654b93d708e6f9119427
Takeru9016 <youthfox046@gmail.com>
2023-06-24 12:28:05 +0530

    Error: Fixing the window not defined

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,33 @@
 "use client";
-
-import { EmptyState } from "@/components";
 import { useEffect } from "react";
 
-interface ErrorStateProps {
+export default function Error({
+  error,
+  reset,
+}: {
   error: Error;
-}
-
-export default function ErrorState({ error }: ErrorStateProps) {
+  reset: () => void;
+}) {
   useEffect(() => {
     console.error(error);
   }, [error]);
-  
-  return <EmptyState title="Uh Oh" subtitle="Something went wrong!" />;
+
+  return (
+    <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
+      <div className="text-center">
+        <div className="text-2xl font-bold">Uh Oh</div>
+        <div className="font-light text-neutral-500 mt-2">
+          Something went wrong!
+        </div>
+      </div>
+      <div className="w-48 mt-4">
+        <button
+          onClick={() => reset()}
+          className="relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full bg-white border-black text-black"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
